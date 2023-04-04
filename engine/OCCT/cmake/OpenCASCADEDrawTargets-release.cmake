c712ed81d9e520b3bc1c91af4b79720a075736d6
CY <xiachenyang53@gmail.com>
2023-04-04 19:10:45 +0800

    added OCCT
    added OCCT

---
+++ engine/OCCT/cmake/OpenCASCADEDrawTargets-release.cmake
@@ -0,0 +1,99 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "TKDraw" for configuration "Release"
+set_property(TARGET TKDraw APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(TKDraw PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/win64/vc14/lib/TKDraw.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/win64/vc14/bin/TKDraw.dll"
+  )
+
+list(APPEND _cmake_import_check_targets TKDraw )
+list(APPEND _cmake_import_check_files_for_TKDraw "${_IMPORT_PREFIX}/win64/vc14/lib/TKDraw.lib" "${_IMPORT_PREFIX}/win64/vc14/bin/TKDraw.dll" )
+
+# Import target "TKTopTest" for configuration "Release"
+set_property(TARGET TKTopTest APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(TKTopTest PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/win64/vc14/lib/TKTopTest.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/win64/vc14/bin/TKTopTest.dll"
+  )
+
+list(APPEND _cmake_import_check_targets TKTopTest )
+list(APPEND _cmake_import_check_files_for_TKTopTest "${_IMPORT_PREFIX}/win64/vc14/lib/TKTopTest.lib" "${_IMPORT_PREFIX}/win64/vc14/bin/TKTopTest.dll" )
+
+# Import target "TKOpenGlTest" for configuration "Release"
+set_property(TARGET TKOpenGlTest APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(TKOpenGlTest PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/win64/vc14/lib/TKOpenGlTest.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/win64/vc14/bin/TKOpenGlTest.dll"
+  )
+
+list(APPEND _cmake_import_check_targets TKOpenGlTest )
+list(APPEND _cmake_import_check_files_for_TKOpenGlTest "${_IMPORT_PREFIX}/win64/vc14/lib/TKOpenGlTest.lib" "${_IMPORT_PREFIX}/win64/vc14/bin/TKOpenGlTest.dll" )
+
+# Import target "TKViewerTest" for configuration "Release"
+set_property(TARGET TKViewerTest APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(TKViewerTest PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/win64/vc14/lib/TKViewerTest.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/win64/vc14/bin/TKViewerTest.dll"
+  )
+
+list(APPEND _cmake_import_check_targets TKViewerTest )
+list(APPEND _cmake_import_check_files_for_TKViewerTest "${_IMPORT_PREFIX}/win64/vc14/lib/TKViewerTest.lib" "${_IMPORT_PREFIX}/win64/vc14/bin/TKViewerTest.dll" )
+
+# Import target "TKXSDRAW" for configuration "Release"
+set_property(TARGET TKXSDRAW APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(TKXSDRAW PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/win64/vc14/lib/TKXSDRAW.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/win64/vc14/bin/TKXSDRAW.dll"
+  )
+
+list(APPEND _cmake_import_check_targets TKXSDRAW )
+list(APPEND _cmake_import_check_files_for_TKXSDRAW "${_IMPORT_PREFIX}/win64/vc14/lib/TKXSDRAW.lib" "${_IMPORT_PREFIX}/win64/vc14/bin/TKXSDRAW.dll" )
+
+# Import target "TKDCAF" for configuration "Release"
+set_property(TARGET TKDCAF APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(TKDCAF PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/win64/vc14/lib/TKDCAF.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/win64/vc14/bin/TKDCAF.dll"
+  )
+
+list(APPEND _cmake_import_check_targets TKDCAF )
+list(APPEND _cmake_import_check_files_for_TKDCAF "${_IMPORT_PREFIX}/win64/vc14/lib/TKDCAF.lib" "${_IMPORT_PREFIX}/win64/vc14/bin/TKDCAF.dll" )
+
+# Import target "TKXDEDRAW" for configuration "Release"
+set_property(TARGET TKXDEDRAW APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(TKXDEDRAW PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/win64/vc14/lib/TKXDEDRAW.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/win64/vc14/bin/TKXDEDRAW.dll"
+  )
+
+list(APPEND _cmake_import_check_targets TKXDEDRAW )
+list(APPEND _cmake_import_check_files_for_TKXDEDRAW "${_IMPORT_PREFIX}/win64/vc14/lib/TKXDEDRAW.lib" "${_IMPORT_PREFIX}/win64/vc14/bin/TKXDEDRAW.dll" )
+
+# Import target "TKTObjDRAW" for configuration "Release"
+set_property(TARGET TKTObjDRAW APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(TKTObjDRAW PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/win64/vc14/lib/TKTObjDRAW.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/win64/vc14/bin/TKTObjDRAW.dll"
+  )
+
+list(APPEND _cmake_import_check_targets TKTObjDRAW )
+list(APPEND _cmake_import_check_files_for_TKTObjDRAW "${_IMPORT_PREFIX}/win64/vc14/lib/TKTObjDRAW.lib" "${_IMPORT_PREFIX}/win64/vc14/bin/TKTObjDRAW.dll" )
+
+# Import target "TKQADraw" for configuration "Release"
+set_property(TARGET TKQADraw APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(TKQADraw PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/win64/vc14/lib/TKQADraw.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/win64/vc14/bin/TKQADraw.dll"
+  )
+
+list(APPEND _cmake_import_check_targets TKQADraw )
+list(APPEND _cmake_import_check_files_for_TKQADraw "${_IMPORT_PREFIX}/win64/vc14/lib/TKQADraw.lib" "${_IMPORT_PREFIX}/win64/vc14/bin/TKQADraw.dll" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
