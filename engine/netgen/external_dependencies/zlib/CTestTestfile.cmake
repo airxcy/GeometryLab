1c8a143437b280224d23a93ff94261b4bed4a07a
xcy <xcy1990@hotmail.com>
2023-04-18 04:03:21 +0800

    clearning netgen

--- engine/netgen/external_dependencies/zlib/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/projects/GeometryLab/engine/netgen/external_dependencies/zlib
-# Build directory: D:/projects/GeometryLab/engine/netgen/external_dependencies/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(example "example")
-set_tests_properties(example PROPERTIES  _BACKTRACE_TRIPLES "D:/projects/GeometryLab/engine/netgen/external_dependencies/zlib/CMakeLists.txt;235;add_test;D:/projects/GeometryLab/engine/netgen/external_dependencies/zlib/CMakeLists.txt;0;")
